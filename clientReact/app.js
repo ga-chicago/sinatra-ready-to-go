4c3880be6001f0e918dd110e9b63815bff71e384
Jim Haff <Jimmyhaff3@gmail.com>
2016-07-27 10:39:16 -0500

    added gulp task

---
+++ clientReact/app.js
@@ -0,0 +1,10 @@
+var React     = require('react');
+var ReactDOM  = require('react-dom');
+
+var form = React.createClass({
+  render: function(){
+    return (
+      <div>hi</div>
+      )
+  }
+})
---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp       = require('gulp');
+    browserify = require('browserify'),
+    babelify   = require('babelify'),
+    source     = require('vinyl-source-stream');
+
+  gulp.task('react', function(){
+  return browserify('./clientReact/app.js')
+          .transform('babelify', {presets: ["react", "es2015"]})
+          .bundle()
+          .pipe(source('build.js'))
+          .pipe(gulp.dest('./app/public/js'))
+})
+
+
+gulp.task('watch', function() {
+  gulp.watch(['./clientReact/*.js'], ['react'])
+});
+
+
+gulp.task('default', ['watch', 'react']);
